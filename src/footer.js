80bf156c7e4aba0dd306c120bc5c189bdebbe068
Luke Preston <lukep@ceh.ac.uk>
2019-07-29 09:00:03 +0100

    created the footer

---
+++ src/footer.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import Acast from './icons/acast'
+import Anchor from './icons/anchor'
+import Breaker from './icons/breaker'
+import CastBox from './icons/cast-box'
+import GooglePodcast from './icons/google-podcast'
+import ITunes from './icons/itunes'
+import Overcast from './icons/overcast'
+import PocketCast from './icons/pocket-cast'
+import Podbean from './icons/podbean'
+import RadioPublic from './icons/radio-public'
+import Spotify from './icons/spotify'
+import Stitcher from './icons/stitcher'
+import TuneIn from './icons/tunein'
+
+export default () => <footer className='footer'>
+  <div className='content'>
+    <i className='icon'>
+      <Anchor />
+    </i>
+    <i className='icon'>
+      <Acast />
+    </i>
+    <i className='icon'>
+      <Breaker />
+    </i>
+    <i className='icon'>
+      <CastBox />
+    </i>
+    <i className='icon'>
+      <GooglePodcast />
+    </i>
+    <i className='icon'>
+      <ITunes />
+    </i>
+    <i className='icon'>
+      <Overcast />
+    </i>
+    <i className='icon'>
+      <PocketCast />
+    </i>
+    <i className='icon'>
+      <Podbean />
+    </i>
+    <i className='icon'>
+      <RadioPublic />
+    </i>
+    <i className='icon'>
+      <Spotify />
+    </i>
+    <i className='icon stitcher'>
+      <Stitcher />
+    </i>
+    <i className='icon tunein'>
+      <TuneIn />
+    </i>
+  </div>
+</footer>
