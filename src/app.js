b0fb29a49aa94cfe81b5c24a674e340d7105585f
Luke Preston <lukep@ceh.ac.uk>
2019-07-30 08:19:22 +0100

    puts the embed at the top of the page

--- src/app.js
+++ src/app.js
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react'
 
 import Hero from './hero'
 import Footer from './footer'
+var parser = require('fast-xml-parser')
 
-export default () => <div className='App'>
-  <header>
-    <Hero />
-  </header>
-  <main>
-    <div className='container'>
+const reducer = (state, { type, payload }) => {
+  if (type === 'fetch-rss') {
+    const link = payload.data.rss.channel.item[0].link.replace('/episodes/', '/embed/episodes/')
+    return { loading: false, link }
+  }
 
-      <iframe
-        src='https://anchor.fm/blank-string/embed/episodes/Blank-String-create-a-Godzilla-to-fight-Godzilla-part-1-of-3-e4hons/a-aibjil'
-        height='102px'
-        width='400px'
-        frameBorder='0'
-        scrolling='no' />
-    </div>
-  </main>
-  <Footer />
-</div>
+  return { loading: true }
+}
+
+const initialState = {
+  loading: true
+}
+
+const fetchRss = async (dispatch) => {
+  const res = await fetch('https://anchor.fm/s/79d0d08/podcast/rss', {
+    mode: 'cors'
+  })
+  const text = await res.text()
+  parser.parse(text)
+  dispatch({ type: 'fetch-rss', payload: { data: parser.parse(text) } })
+}
+
+export default () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    if (state.loading) fetchRss(dispatch)
+  })
+
+  return <div className='App'>
+    <header>
+      <Hero />
+    </header>
+    <main>
+      <div className='container'>
+        {state.link
+          ? <iframe
+            title='Blank String embed'
+            src={state.link}
+            scrolling='no' />
+          : null}
+      </div>
+    </main>
+    <Footer />
+  </div>
+}
