42e1ffa0ce41e495ed760a262a84bee78a7d464d
Luke Preston <lukep@ceh.ac.uk>
2019-08-02 07:53:17 +0100

    putting the hosts at the top of the page

--- src/hero.js
+++ src/hero.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import BlankString from './icons/blank-string'
 
+import Hosts from './hosts'
+
 export default () => <section className='hero'>
   <div className='hero-body'>
     <div className='container'>
@@ -18,4 +20,5 @@ export default () => <section className='hero'>
       </div>
     </div>
   </div>
+  <Hosts />
 </section>
