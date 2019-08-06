477c7c852e047c889a86f1002880e8266b8df083
Luke Preston <lukep@ceh.ac.uk>
2019-08-06 09:42:27 +0100

    Updates

---
+++ precache-manifest.d85aee068fe1d4b5c560af67ae40cefc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed37d8060bd4ccc14564a0f68d61cca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5de5cf0e4212f378cdfc",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "5ac7400b25cfd15daf46",
+    "url": "/static/css/main.a13b9c14.chunk.css"
+  },
+  {
+    "revision": "5de5cf0e4212f378cdfc",
+    "url": "/static/js/2.e7a666a3.chunk.js"
+  },
+  {
+    "revision": "5ac7400b25cfd15daf46",
+    "url": "/static/js/main.68f0e648.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
