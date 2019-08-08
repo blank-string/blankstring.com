57ed91f73d12d43b85ca90cb8d175ff2bed8ce4f
Luke Preston <lukep@ceh.ac.uk>
2019-08-08 13:04:26 +0100

    adding meta info

--- src/distribution-single.js
+++ src/distribution-single.js
@@ -1,28 +1,39 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import DistributionLink from './components/distribution-link'
 
 export default ({ state, match }) => {
   let found = state.distribution.filter(({ name }) => name === match.params.distribution)
   found = found[0] || state.distribution[0]
-  return <section className='section'>
-    <div className='container'>
-      <div className='distribution-hero-text-container'>
-        <div className='distribution-hero-text'>
-          <pre className='distribution-hero-text-left'>{`Go listen to the
+  return <>
+    <Helmet>
+      <title>{`Blank String Podcast ${found.title}`}</title>
+      <meta name='title' content={`Blank String Podcast ${found.title}`} />
+      <meta name='og:title' content={`Blank String Podcast ${found.title}`} />
+      <meta name='description' content={`You can find Blank String at ${found.title}`} />
+      <meta name='og:description' content={`You can find Blank String at ${found.title}`} />
+      <meta name='og:url' content={found.href} />
+    </Helmet>
+    <section className='section'>
+      <div className='container'>
+        <div className='distribution-hero-text-container'>
+          <div className='distribution-hero-text'>
+            <pre className='distribution-hero-text-left'>{`Go listen to the
 well designed`}</pre>
-          <p className='distribution-hero-text-right'>PODCAST</p>
+            <p className='distribution-hero-text-right'>PODCAST</p>
+          </div>
         </div>
+        <ul>
+          <li key={found.name}>
+            <DistributionLink
+              link={false}
+              name={found.name}
+              href={found.href}
+              title={found.title} />
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li key={found.name}>
-          <DistributionLink
-            link={false}
-            name={found.name}
-            href={found.href}
-            title={found.title} />
-        </li>
-      </ul>
-    </div>
-  </section>
+    </section>
+  </>
 }
--- src/distribution.js
+++ src/distribution.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import DistributionLink from './components/distribution-link'
 
-export default ({ state }) => <section className='section'>
-  <div className='container'>
-    <p className='subtitle has-text-centered'>You can find Blank String on the following platforms</p>
-    <ul>
-      {state.distribution.map(({ name, href, title }) => <li key={name}>
-        <DistributionLink
-          name={name}
-          href={href}
-          title={title} />
-      </li>)}
-    </ul>
-  </div>
-</section>
+export default ({ state }) => <>
+  <Helmet>
+    <title>{`Blank String Podcast Distribution`}</title>
+    <meta name='title' content='Blank String Podcast Distribution' />
+    <meta name='og:title' content='Blank String Podcast Distribution' />
+    <meta name='description' content='You can find Blank String on the following platforms' />
+    <meta name='og:description' content='You can find Blank String on the following platforms' />
+    <meta name='og:url' content='https://blankstring.com' />
+  </Helmet>
+  <section className='section'>
+    <div className='container'>
+      <p className='subtitle has-text-centered'>You can find Blank String on the following platforms</p>
+      <ul>
+        {state.distribution.map(({ name, href, title }) => <li key={name}>
+          <DistributionLink
+            name={name}
+            href={href}
+            title={title} />
+        </li>)}
+      </ul>
+    </div>
+  </section>
+</>
